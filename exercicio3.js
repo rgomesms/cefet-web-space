bf77119c8f68e6f8e196719d76be169313165357
rgomesms <rgomesms227@gmail.com>
2021-06-19 19:10:56 -0300

    exercicio 3

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,13 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+function getElements(querySelector) {
+	return document.querySelectorAll(querySelector)
+}
+let buttons = getElements(".botao-expandir-retrair");
+buttons.forEach(button => { button.addEventListener('click', expandableButtonClick) });
+
+function expandableButtonClick(e) {
+	let clickedElement = e.currentTarget;
+	let parentElement = clickedElement.parentNode;
+	parentElement.classList.toggle('expandido');
+}
