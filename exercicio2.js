94c453575b8b9fe04017b2a27acb5c65d9858b06
rgomesms <rgomesms227@gmail.com>
2021-06-19 19:10:46 -0300

    exercicio 2

--- exercicio2.js
+++ exercicio2.js
@@ -4,47 +4,80 @@
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/osiris';
 const imagens = [
     {
-      arquivo: 'modelo-artistico-da-sonda.webp',
-      descricao: 'Imagem em 3D mostrando a sonda tocando um asteróide. ' +
-        'A sonda tem forma de caixa com dois painéis solares em suas ' +
-        'laterais e, na parte de baixo, um pequeno braço sai e encosta ' +
-        'no asteróide'
+        arquivo: 'modelo-artistico-da-sonda.webp',
+        descricao: 'Imagem em 3D mostrando a sonda tocando um asteróide. ' +
+            'A sonda tem forma de caixa com dois painéis solares em suas ' +
+            'laterais e, na parte de baixo, um pequeno braço sai e encosta ' +
+            'no asteróide'
     },
     {
-      arquivo: 'lancamento-atlas-v-em-2017.webp',
-      descricao: 'Foto de foguete na vertical no momento de seu ' +
-        'lançamento. Ele possui bandeiras da NASA, dos EUA e da empresa ' +
-        'ULA e está com o motor sendo ligado, saindo fogo e fumaça'
+        arquivo: 'lancamento-atlas-v-em-2017.webp',
+        descricao: 'Foto de foguete na vertical no momento de seu ' +
+            'lançamento. Ele possui bandeiras da NASA, dos EUA e da empresa ' +
+            'ULA e está com o motor sendo ligado, saindo fogo e fumaça'
     },
     {
-      arquivo: 'distanciamento-da-sonda-da-terra.webp',
-      descricao: 'Foto do espaço quase toda preta mostrando a Terra bem ' +
-        'pequena no canto esquerdo e, no canto direito, a lua ainda menor'
+        arquivo: 'distanciamento-da-sonda-da-terra.webp',
+        descricao: 'Foto do espaço quase toda preta mostrando a Terra bem ' +
+            'pequena no canto esquerdo e, no canto direito, a lua ainda menor'
     },
     {
-      arquivo: 'aproximacao-da-sonda-ao-bennu-em-2018.webp',
-      descricao: 'Foto do espaço mostrando o asteróide Bennu que tem ' +
-        'a forma de um paralelepípedo com base losangular aproximadamente ' +
-        'e a superfície acinzentada'
+        arquivo: 'aproximacao-da-sonda-ao-bennu-em-2018.webp',
+        descricao: 'Foto do espaço mostrando o asteróide Bennu que tem ' +
+            'a forma de um paralelepípedo com base losangular aproximadamente ' +
+            'e a superfície acinzentada'
     },
     {
-      arquivo: 'local-de-pouso.webp',
-      descricao: 'Foto do local de pouso da sonda Osiris-Rex no ' +
-        'asteróide Bennu mostrando uma pequena clareira em um ' +
-        'terreno bem acidentado, com muitas pedras. No centro há ' +
-        'um desenho no formato da sonda'
+        arquivo: 'local-de-pouso.webp',
+        descricao: 'Foto do local de pouso da sonda Osiris-Rex no ' +
+            'asteróide Bennu mostrando uma pequena clareira em um ' +
+            'terreno bem acidentado, com muitas pedras. No centro há ' +
+            'um desenho no formato da sonda'
     },
     {
-      arquivo: 'touch-and-go-e-detritos-suspensos-em-2020.webp',
-      descricao: 'Foto do asteróide Bennu com vários regolitos (pedras) ' +
-        'sendo suspensos após a sonda ter encostado para coletar amostras ' +
-        'da sua superfície'
+        arquivo: 'touch-and-go-e-detritos-suspensos-em-2020.webp',
+        descricao: 'Foto do asteróide Bennu com vários regolitos (pedras) ' +
+            'sendo suspensos após a sonda ter encostado para coletar amostras ' +
+            'da sua superfície'
     },
     {
-      arquivo: 'sonda-distanciando-e-voltando-para-terra.webp',
-      descricao: 'Foto do asteróide Bennu com ele retratado em ' +
-        'menos da metade e com baixa iluminação, mostrando sua superfície ' +
-        'acinzentada'
+        arquivo: 'sonda-distanciando-e-voltando-para-terra.webp',
+        descricao: 'Foto do asteróide Bennu com ele retratado em ' +
+            'menos da metade e com baixa iluminação, mostrando sua superfície ' +
+            'acinzentada'
     }
-  ];
+];
 
+
+function getElement(querySelector) {
+    return document.querySelector(querySelector)
+}
+
+let currentImageIndex = 0;
+let anteriorButton = getElement("#anterior");
+let proximoButton = getElement("#proximo");
+
+anteriorButton.addEventListener('click', () => changeImage(-1));
+proximoButton.addEventListener('click', () => changeImage(1));
+
+function changeImage(offset) {
+    function getNextImageIndex(offset) {
+        /* Dado que:
+            (index + length) % length = index
+            (offset % length), para length de 8, retorna um valor entre -7 e 7.
+           Podemos juntar os dois abaixo, para evitar qualquer comparação.
+        */
+        const length = imagens.length;
+        let nextIndex = (currentImageIndex + length + offset % length) % length
+        return nextIndex
+    }
+    function getImageLink(fileUrl) {
+        return servidorDasImagens + "/" + fileUrl
+    }
+
+    currentImageIndex = getNextImageIndex(offset);
+
+    let slideElement = getElement('#slide')
+    slideElement.alt = imagens[currentImageIndex].descricao;
+    slideElement.src = getImageLink(imagens[currentImageIndex].arquivo);
+}
