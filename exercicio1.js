9d13e34726bd065056776c9b1d906ac2aed2b660
rgomesms <rgomesms227@gmail.com>
2021-06-19 19:10:11 -0300

    exercicio 1

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,21 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let calcularButton = getElement('#calcular')
+calcularButton.addEventListener('click', calcular);
+
+function getElement(querySelector) {
+	return document.querySelector(querySelector)
+}
+
+function calcular() {
+	let G = getElement("#constante").value;
+	let M1 = getElement("#massa1").value;
+	let M2 = getElement("#massa2").value;
+	let d = getElement("#distancia").value;
+	let resultadoElement = getElement('#resultado');
+
+	if (G && M1 && M2 && d)
+		resultadoElement.value = (G * M1 * M2) / (d * d);
+}
+
